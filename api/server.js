374fc233e488f182960aa75f6ddd463c76423caf
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-06-10 16:17:32 -0500

    basic file structure setup

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('expres');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const userRouter = require('../users/userRouter.js');
+const authRouter = require('../auth/auth-router.js')
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+
+server.use('/api/users', userRouter);
+server.use('/api/auth', authRouter);
+
+
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,8 @@
+const server = require('./api/server.js');
+
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`Server is listening on ${port}`)
+})
