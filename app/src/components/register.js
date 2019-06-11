fedb1e6c6a3e1135f8e050d5e5004abdeb3773af
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-06-11 15:48:10 -0500

    added store

---
+++ app/src/components/register.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import
+
+
+class Register extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return(
+
+        )
+    }
+}
+
+
+export default Register;
--- auth/restricted.js
+++ auth/restricted.js
@@ -6,7 +6,6 @@ module.exports = function restricted(req, res, next) {
     if (req.session && req.session.user) {
        next();
     } else {
-        console.log(req.session, req.session.user)
         res.status(401).json({ message: 'You shall not pass.'})
     }
 }
